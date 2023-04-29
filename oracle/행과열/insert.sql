875df784c10c27492fcf7996d98907b5450f9b49
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-29 14:25:14 +0900

    Create insert.sql

---
+++ oracle/행과열/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO topic(id, title, description, created)
+VALUES (1,'ORACLE','ORACLE is ...', SYSDATE);
+
+INSERT INTO topic(id, title, description, created)
+VALUES (2,'MySQL','MySQL is ...', SYSDATE);
+
+INSERT INTO topic(id, title, description, created)
+VALUES (3,'SQL Server','SQL Server is ...', SYSDATE);
